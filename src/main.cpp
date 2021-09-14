e2e737f81d61a520400d80d0308deb434d473e3d
hophiphip <nighop@yandex.ru>
2021-09-14 14:02:46 +0400

    Added Arduino-based branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,11 @@
-//#include "esp_system.h"
-//#include "esp_spi_flash.h"
-//#include "esp_log.h" // ESP_LOGE
-//
-//#include <stdio.h>
-//#include "freertos/FreeRTOS.h"
-//#include "freertos/task.h"
-
 extern "C" {
   #include "esp_camera.h"
   #include "camera_model.h"
   #include "camera_pins.h"
 }
+
 #include <Arduino.h>
+#include <WiFi.h>
 
 static const char *TAG = "main.c";
 
@@ -57,37 +51,6 @@ esp_err_t camera_capture()
 
     return ESP_OK;
 }
-
-//void app_main()
-//{
-//    printf("Hello world!\n");
-//
-//
-//    esp_err_t err = ESP_OK;
-//    err = camera_init();
-//    err = camera_capture();
-//
-//    /* Print chip information */
-//    esp_chip_info_t chip_info;
-//    esp_chip_info(&chip_info);
-//    printf("This is ESP32 chip with %d CPU cores, WiFi%s%s, ",
-//            chip_info.cores,
-//            (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-//            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
-//
-//    printf("silicon revision %d, ", chip_info.revision);
-//
-//    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-//            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
-//
-//    for (int i = 10; i >= 0; i--) {
-//        printf("Restarting in %d seconds...\n", i);
-//        vTaskDelay(1000 / portTICK_PERIOD_MS);
-//    }
-//    printf("Restarting now.\n");
-//    fflush(stdout);
-//    esp_restart();
-//}
 
 void setup() {
   Serial.begin(115200);
